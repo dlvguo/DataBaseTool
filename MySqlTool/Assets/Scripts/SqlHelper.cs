122982081b0f657de2c41cd81567b5d358337e5c
ChenJG <dlvguo@qq.com>
2020-01-30 18:59:38 +0800

    1

--- MySqlTool/Assets/Scripts/SqlHelper.cs
+++ MySqlTool/Assets/Scripts/SqlHelper.cs
@@ -2,45 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 using MySql.Data;
-using MySql.Data.MySqlClient;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+//Mysql使用这个
+//using MySql.Data.MySqlClient; 并且把所有Sql换成MySql
 using System;
 
-public class SqlHelper {
+public class SqlHelper
+{
     private static SqlHelper _instance = new SqlHelper();
-    private static string connStr="Database=Lwcc;Data Source = 172.22.224.87; User Id = root; Password=123456;port=3306";
+    //Mysql的连接方法
+    //private static string connStr="Database=Lwcc;Data Source = 172.22.224.87; User Id = root; Password=123456;port=3306";
+    //SqlServer方法
+    private static string connStr = "server=106.15.121.111;database=GameDB;uid=sa;pwd=a2241939438...";
+
     private string ip;
     private string acc;
     private string psd;
     private string db;
-    public static SqlHelper Insatance { get
+    public static SqlHelper Insatance
+    {
+        get
         {
             return _instance;
-        } }
+        }
+    }
+    
+
 
     /// <summary>
     /// 获取连接
     /// </summary>
     /// <returns></returns>
-    private MySqlConnection GetSqlConn()
+    private SqlConnection GetSqlConn()
     {
-        MySqlConnection sqlConn = null;
-        sqlConn = new MySqlConnection(connStr);
+        SqlConnection sqlConn = null;
+        sqlConn = new SqlConnection(connStr);
         return sqlConn;
     }
-	
+
     /// <summary>
     /// 获取所有表名
     /// </summary>
     /// <returns></returns>
     public IEnumerable<string> GetTables()
     {
-        string sql = string.Format(@"select table_name from information_schema.tables where table_schema='{0}' and table_type='base table'", db);
+        string sql = string.Format(@"Select Name FROM SysObjects Where XType='U' orDER BY Name;", db);
         //string sql = @"select table_name from information_schema.tables where table_schema='lwcc' and table_type='base table'";
         List<string> strs = new List<string>();
         var conn = GetSqlConn();
         conn.Open();
-        MySqlCommand cmd = new MySqlCommand(sql, conn);
-        MySqlDataReader reader = cmd.ExecuteReader();
+        SqlCommand cmd = new SqlCommand(sql, conn);
+        SqlDataReader reader = cmd.ExecuteReader();
         while (reader.Read())
         {
             strs.Add(reader[0].ToString());
@@ -56,27 +70,70 @@ public class SqlHelper {
     /// <param name="acc"></param>
     /// <param name="psd"></param>
     /// <param name="db"></param>
-    public void SetConnStr(string ip,string acc,string psd,string db)
+    public void SetConnStr(string ip, string acc, string psd, string db)
     {
         this.ip = ip;
         this.acc = acc;
         this.psd = psd;
         this.db = db;
-        connStr = string.Format("DataBase={0};Data Source= {1};User Id = {2};Password={3};port=3306;", db, ip, acc, psd);
+        //server = 106.15.121.111; database = GameDB; uid = sa; pwd = a2241939438...
+        //connStr = string.Format("DataBase={0};Data Source= {1};User Id = {2};Password={3};port=3306;", db, ip, acc, psd);
+        connStr = string.Format("server={0};database= {1};uid = {2};pwd={3};", ip, db, acc, psd);
     }
 
     public void ClearTables(IEnumerable<string> tbs)
     {
         string cmdText = "";
-        foreach(var item in tbs)
+        foreach (var item in tbs)
         {
             cmdText += string.Format("truncate table {0};", item);
         }
         var conn = GetSqlConn();
         conn.Open();
-        MySqlCommand cmd = new MySqlCommand(cmdText,conn);
+        SqlCommand cmd = new SqlCommand(cmdText, conn);
         cmd.ExecuteNonQuery();
         conn.Close();
     }
 
+    /// <summary>
+    /// 测试连接
+    /// </summary>
+    /// <returns></returns>
+    public string TestConn()
+    {
+        var conn = GetSqlConn();
+        try
+        {
+            conn.Open();
+            conn.Close();
+            return "数据库连接成功";
+        }
+
+        catch
+        {
+            throw;
+        }
+    }
+
+
+    /// <summary>
+    /// 插入数据
+    /// </summary>
+    /// <param name="tableName"></param>
+    /// <param name="values"></param>
+    public void Insert(string tableName, string values,string vars="")
+    {
+        string sql = string.Format("INSERT INTO {0} {1} VALUES ({2});", tableName,vars, values);
+        var conn = GetSqlConn();
+        conn.Open();
+        SqlCommand cmd = new SqlCommand(sql, conn);
+        cmd.ExecuteNonQuery();
+        conn.Close();
+    }
+
+    //更新
+    public void Update()
+    {
+
+    }
 }
---
+++ MySqlTool/Assets/Scripts/UI/Tips.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//提示消息
+public class Tips : MonoBehaviour {
+
+    public static Tips Instance { get; private set; }
+    private Text info;
+    private Button confirm;
+    void Start () {
+        Instance = this;
+        info = transform.Find("Info").GetComponent<Text>();
+        confirm = transform.Find("confirm").GetComponent<Button>();
+        confirm.onClick.AddListener(() => { gameObject.SetActive(false); });
+        gameObject.SetActive(false);
+	}
+	
+    public void OnException(Exception e)
+    {
+        gameObject.SetActive(true);
+        info.text = e.Message + e.InnerException;
+    }
+
+    public void OnSuccess(string str)
+    {
+        gameObject.SetActive(true);
+        info.text = str;
+    }
+}

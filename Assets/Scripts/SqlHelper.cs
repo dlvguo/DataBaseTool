87d3aa232b6a1bac9bd9104414037061d6210c9a
ChenJG <dlvguo@qq.com>
2020-01-31 11:10:03 +0800

    1

---
+++ Assets/Scripts/SqlHelper.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MySql.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+//Mysql使用这个
+//using MySql.Data.MySqlClient; 并且把所有Sql换成MySql
+using System;
+
+public class SqlHelper
+{
+    private static SqlHelper _instance = new SqlHelper();
+    //Mysql的连接方法
+    //private static string connStr="Database=Lwcc;Data Source = 172.22.224.87; User Id = root; Password=123456;port=3306";
+    //SqlServer方法
+    private static string connStr = "server=106.15.121.111;database=GameDB;uid=sa;pwd=a2241939438...";
+
+    private string ip;
+    private string acc;
+    private string psd;
+    private string db;
+    public static SqlHelper Insatance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+    
+
+
+    /// <summary>
+    /// 获取连接
+    /// </summary>
+    /// <returns></returns>
+    private SqlConnection GetSqlConn()
+    {
+        SqlConnection sqlConn = null;
+        sqlConn = new SqlConnection(connStr);
+        return sqlConn;
+    }
+
+    /// <summary>
+    /// 获取所有表名
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerable<string> GetTables()
+    {
+        string sql = string.Format(@"Select Name FROM SysObjects Where XType='U' orDER BY Name;", db);
+        //string sql = @"select table_name from information_schema.tables where table_schema='lwcc' and table_type='base table'";
+        List<string> strs = new List<string>();
+        var conn = GetSqlConn();
+        conn.Open();
+        SqlCommand cmd = new SqlCommand(sql, conn);
+        SqlDataReader reader = cmd.ExecuteReader();
+        while (reader.Read())
+        {
+            strs.Add(reader[0].ToString());
+        }
+        conn.Close();
+        return strs;
+    }
+
+    /// <summary>
+    /// 设置连接Str
+    /// </summary>
+    /// <param name="ip"></param>
+    /// <param name="acc"></param>
+    /// <param name="psd"></param>
+    /// <param name="db"></param>
+    public void SetConnStr(string ip, string acc, string psd, string db)
+    {
+        this.ip = ip;
+        this.acc = acc;
+        this.psd = psd;
+        this.db = db;
+        //server = 106.15.121.111; database = GameDB; uid = sa; pwd = a2241939438...
+        //connStr = string.Format("DataBase={0};Data Source= {1};User Id = {2};Password={3};port=3306;", db, ip, acc, psd);
+        connStr = string.Format("server={0};database= {1};uid = {2};pwd={3};", ip, db, acc, psd);
+    }
+
+    public void ClearTables(IEnumerable<string> tbs)
+    {
+        string cmdText = "";
+        foreach (var item in tbs)
+        {
+            cmdText += string.Format("truncate table {0};", item);
+        }
+        var conn = GetSqlConn();
+        conn.Open();
+        SqlCommand cmd = new SqlCommand(cmdText, conn);
+        cmd.ExecuteNonQuery();
+        conn.Close();
+    }
+
+    /// <summary>
+    /// 测试连接
+    /// </summary>
+    /// <returns></returns>
+    public string TestConn()
+    {
+        var conn = GetSqlConn();
+        try
+        {
+            conn.Open();
+            conn.Close();
+            return "数据库连接成功";
+        }
+
+        catch
+        {
+            throw;
+        }
+    }
+
+
+    /// <summary>
+    /// 插入数据
+    /// </summary>
+    /// <param name="tableName"></param>
+    /// <param name="values"></param>
+    public void Insert(string tableName, string values,string vars="")
+    {
+        string sql = string.Format("INSERT INTO {0} {1} VALUES ({2});", tableName,vars, values);
+        var conn = GetSqlConn();
+        conn.Open();
+        SqlCommand cmd = new SqlCommand(sql, conn);
+        cmd.ExecuteNonQuery();
+        conn.Close();
+    }
+
+    //更新
+    public void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/UI/Ribbon.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public enum PrefabType
+{
+    Input,
+    Button
+};
+
+
+//功能区域
+
+public class Ribbon : MonoBehaviour
+{
+
+    //单例
+    private static Ribbon _Instance = new Ribbon();
+
+    [SerializeField]
+    private GameObject buttonPrefab;
+
+    public GameObject ButtonPrefab
+    {
+        get
+        {
+            return buttonPrefab;
+        }
+    }
+    [SerializeField]
+    private GameObject inputField;
+    public GameObject InputField
+    {
+        get
+        {
+            return inputField;
+        }
+    }
+
+
+    public static Ribbon Instance
+    {
+        get
+        {
+            return _Instance;
+        }
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+        _Instance = this;
+    }
+
+
+    //清空
+    public void ClearRibbon()
+    {
+
+        int childCount = this.GetComponent<RectTransform>().childCount;
+        for (int i = 1; i < childCount; i++)
+        {
+            Destroy(transform.GetChild(i).gameObject);
+        }
+
+    }
+
+    public void CreatPrefab(PrefabType type, Vector3 pos, string Info)
+    {
+
+    }
+}
